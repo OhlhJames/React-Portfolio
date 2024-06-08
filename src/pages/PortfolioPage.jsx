c7f14cff63beefa9179b62adbe8ab0b7fccb39d0
IsItIzzet <ohlhjames@gmail.com>
2024-06-08 16:41:00 -0500

    added PortfolioPage as well as some starter info and set up project component to be added and used so that projects can be displayed individually

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,9 +1,40 @@
+import Project from "../components/Project"
 export default function PortfolioPage() {
-    return(
-        <div>
-            <p>
-                2
-            </p>
+    const projects = []
+    const project1 = {
+        title: "Crop Club",
+        github: " ",
+        link: " ",
+        img: " "
+    }
+    const project2 = {
+        title: "CorknFork",
+        github: " ",
+        link: " ",
+        img: " "
+    }
+    const project3 = {
+        title: "Honkai Calculator",
+        github: " ",
+        link: " ",
+        img: " "
+    }
+    projects.push(project1)
+    projects.push(project2)
+    projects.push(project3)
+    return (
+        <div className="my-2">
+          <h2>Our Products:</h2>
+            <div className="flex-row">
+              {projects.map((project) => (
+                <Project
+                  img={project.img}
+                  title={project.title}
+                  link={project.link}
+                  github={project.github}
+                />
+              ))}
+            </div>
         </div>
     )
 }
