a772251687070c4d65ff64d8c5d17dd5f8352709
IsItIzzet <ohlhjames@gmail.com>
2024-06-08 16:29:47 -0500

    Added info to contact page and about page as well as adding styling components to each page so that users can easily understand the info

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,15 +1,14 @@
 export default function AboutPage() {
     return (
-      <div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-          tenetur maiores, dolor iusto dolorum ullam, natus deleniti blanditiis
-          impedit suscipit sed magnam alias in, repellat expedita hic explicabo
-          architecto soluta. About us Lorem ipsum, dolor sit amet consectetur
-          adipisicing elit. Velit voluptate exercitationem quaerat pariatur
-          mollitia, excepturi, voluptatem eveniet a dolor nobis ex veniam totam
-          nostrum temporibus ad omnis nam rerum eligendi.
-        </p>
+      <div class = "container">
+        <div class = "about">
+          <p>
+            Hello! My name is James Ohlhausen and I and a soon to be graduate from the Full Stack Web Development Bootcamp at UT in Austin, Texas.
+            I started my coding roots in highschool where I was the captain of the UIL Computer Science Team in Junior and Senior year.
+            I learned basic JavaScript skills with functions and debugging as well as learned a good amount about how code is read as well as how the Stack operates.
+            I am mostly experienced in JavaScript, HTML, CSS for my base languages but I have also dabbled in Python for some small personal projects.
+          </p>
+        </div>
       </div>
     );
   }
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,7 +1,18 @@
 export default function ContactPage () {
     return(
-        <div>
-            <p>1</p>
+        <div class = "container">
+            <div class = "card">
+                <h1>Contact Me!</h1>
+            </div>
+            <div class = "card">
+                <h2>ohlhjames@gmail.com</h2>
+            </div>
+            <div class = "card">
+                <h2>210-336-5244</h2>
+            </div>
+            <div class = "card">
+                <a class = "linkedin" href = "https://www.linkedin.com/in/james-ohlhausen-912917271/">OhlhJames</a>
+            </div>
         </div>
     ) 
 }
