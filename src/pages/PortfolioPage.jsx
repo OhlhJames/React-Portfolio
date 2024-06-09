ac4b94f0e057ec361c0c2109732a616e4c2f9d3d
IsItIzzet <ohlhjames@gmail.com>
2024-06-09 16:24:59 -0500

    continous changes as netlify only sees git pushed

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -5,19 +5,19 @@ export default function PortfolioPage() {
             title: "Crop Club",
             github: "https://github.com/OhlhJames/Crop-Club",
             link: "https://cropclub-5cbdffda2ae5.herokuapp.com/",
-            img: "../../../public/images/corn.jpeg"
+            img: "../../images/corn.jpeg"
         },
         {
             title: "CorknFork",
             github: "https://github.com/kleppy/corknfork",
             link: " ",
-            img: "../../../public/images/sharp-cheddar-baby-swiss-edited.jpg "
+            img: "../../images/sharp-cheddar-baby-swiss-edited.jpg "
         },
         {
             title: "Honkai Calculator",
             github: "https://github.com/OhlhJames/HSR-Calculator",
             link: "https://ohlhjames.github.io/HSR-Calculator/",
-            img: "../../../public/images/calculations.jpg "
+            img: "../../images/calculations.jpg "
         },
     ]
 
